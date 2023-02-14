2fa356ee02ce98ee94810af798a5466b3fe6c4e0
sudeer48 <sudeer0408@gmail.com>
2023-02-14 18:59:24 +0530

    dsfsd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,6 @@ import { LoginComponent } from './login/login.component'
     HomeComponent,
     DetailsComponent,
     StudentCreateComponent,
-    PopUpComponent,
     LoginComponent
   ],
   imports: [
