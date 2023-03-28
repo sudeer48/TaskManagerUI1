2d4f6fc0787cd053a26f4c41d0efc2c3646f8de5
sudeer48 <sudeer0408@gmail.com>
2023-03-28 18:17:16 +0530

    Added Interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component'
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
@@ -15,6 +15,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { DialogModule } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { JwtinterceptorInterceptor } from './helpers/jwtinterceptor.interceptor';
 
 
 
